d665e0e4bbb14e2d182bd6efaa754765f81fb694
Eduardo Vital <vitaldu@gmail.com>
2016-04-06 16:45:20 -0300

    Added User seed
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias InspecApi.Repo
+alias InspecApi.V1.User
+
+[
+  %User{
+    name: "Eduardo",
+    email: "eduardo@mail.com"
+  },
+  %User{
+    name: "Lucas",
+    email: "lucas@mail.com"
+  }
+] |> Enum.each(&Repo.insert!(&1))
+
